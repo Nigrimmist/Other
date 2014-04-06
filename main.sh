1130df6dbd8ff2559e1bb1e55647069813ea27cb
Nigrimmist <Nigrimmist@gmail.com>
2014-04-06 03:15:06 +0300

    -m

--- main.sh
+++ main.sh
@@ -14,10 +14,10 @@ sudo apt-get update
 sudo apt-get install xrdp #rdp
 
 #download configs
-wget -O root/www/configs/lsyncd.conf.lua "https://raw.githubusercontent.com/Nigrimmist/Other/master/Configs/lsyncd.conf.lua"
+wget -O /root/www/configs/lsyncd.conf.lua "https://raw.githubusercontent.com/Nigrimmist/Other/master/Configs/lsyncd.conf.lua"
 
 #download scripts
-wget -O root/www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
-chmod +x root/www/scripts/lsyncd.sh
+wget -O /root/www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
+chmod +x /root/www/scripts/lsyncd.sh
 sudo root/www/scripts/lsyncd.sh
 
