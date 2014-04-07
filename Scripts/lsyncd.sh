e04c9d647c68c52e24a4d4dc8cdb1c0d4b5c0bba
Nigrimmist <Nigrimmist@gmail.com>
2014-04-07 16:36:47 +0300

    -

--- Scripts/lsyncd.sh
+++ Scripts/lsyncd.sh
@@ -1,8 +1,9 @@
+
 sudo apt-get install lsyncd
 echo "backupServer $backup_server_ip" >> /etc/hosts
 
 cp -i root/www/configs/lsyncd.conf.lua /etc/lsyncd/
-echo "!!!ENTER PATH : root/www/ssh/id_rsa"
+echo "!!!press enter here"
 ssh-keygen -t rsa
 ssh-copy-id "root@$backup_server_ip"
-echo "!!!PLEASE check ssh connect ssh 'root@'"+$backup_server_ip+"'"
+echo "!!!PLEASE check ssh connect ssh 'root@$backup_server_ip'"
--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 export backup_server_ip="188.226.186.242"
 
-mkdir root/www
-mkdir root/www/configs
-mkdir root/www/logs
-mkdir root/www/scripts
+
+mkdir /root/www
+mkdir /root/www/configs
+mkdir /root/www/logs
+mkdir /root/www/scripts
 mkdir /root/www/gifs
 mkdir /root/www/ssh
 mkdir /etc/lsyncd
 
 
-sudo su
+
 sudo apt-get update
 sudo apt-get install xrdp #rdp
 
@@ -19,5 +20,5 @@ wget -O /root/www/configs/lsyncd.conf.lua "https://raw.githubusercontent.com/Nig
 #download scripts
 wget -O /root/www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
 chmod +x /root/www/scripts/lsyncd.sh
-sudo root/www/scripts/lsyncd.sh
+sudo /root/www/scripts/lsyncd.sh backup_server_ip=$backup_server_ip
 
