ac619b8a03eeb667c72b79c856dd00afe9bc184f
Nigrimmist <Nigrimmist@gmail.com>
2014-04-09 16:24:41 +0300

    1

--- Scripts/lsyncd.sh
+++ Scripts/lsyncd.sh
@@ -3,7 +3,14 @@ sudo apt-get install lsyncd
 echo "backupServer $backup_server_ip" >> /etc/hosts
 
 cp -i root/www/configs/lsyncd.conf.lua /etc/lsyncd/
+cp -i root/www/configs/lsyncd.conf /etc/init/
 echo "!!!press enter here"
 ssh-keygen -t rsa
 ssh-copy-id "root@$backup_server_ip"
 echo "!!!PLEASE check ssh connect ssh 'root@$backup_server_ip'"
+
+ln -s /lib/init/upstart-job /etc/init.d/lsyncd
+
+chmod 775 /etc/init.d/lsyncd
+chown root:root /etc/init.d/lsyncd
+service lsyncd start
--- main.sh
+++ main.sh
@@ -17,8 +17,13 @@ sudo apt-get install xrdp #rdp
 #download configs
 wget -O /root/www/configs/lsyncd.conf.lua "https://raw.githubusercontent.com/Nigrimmist/Other/master/Configs/lsyncd.conf.lua"
 
+wget -O /root/www/configs/lsyncd.conf "https://raw.githubusercontent.com/Nigrimmist/Other/master/Configs/lsyncd.conf"
+
 #download scripts
 wget -O /root/www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
 chmod +x /root/www/scripts/lsyncd.sh
+
+
+
 sudo /root/www/scripts/lsyncd.sh backup_server_ip=$backup_server_ip
 
