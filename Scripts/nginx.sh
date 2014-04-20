4a334140713c588e0d2f4ba14ce9d636a91d4e49
Nigrimmist <Nigrimmist@gmail.com>
2014-04-20 23:46:43 +0300

    -

--- Scripts/nginx.sh
+++ Scripts/nginx.sh
@@ -2,24 +2,25 @@ wget -O /www/configs/nginx.conf "https://raw.githubusercontent.com/Nigrimmist/Ot
 
 sudo apt-get install nginx
 
-wget -O /www/temp "http://luajit.org/download/LuaJIT-2.0.1.tar.gz"
-cd /www/temp/ && tar -xzvf /www/temp/LuaJIT-2.0.1.tar.gz
+wget -O /www/temp/LuaJIT-2.0.1.tar.gz "http://luajit.org/download/LuaJIT-2.0.1.tar.gz"
+cd /www/temp && tar -xzvf /www/temp/LuaJIT-2.0.1.tar.gz
 
-cd LuaJIT-2.0.1
+
 sudo apt-get install gcc
-sudo make && sudo make install
+cd /www/temp/LuaJIT-2.0.1 && sudo make
+cd /www/temp/LuaJIT-2.0.1 && sudo make install
 
 
-wget -O /www/temp "https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz"
-cd /www/temp/ && tar -xzvf /www/temp/v0.2.19.tar.gz
+wget -O /www/temp/v0.2.19.tar.gz "https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz"
+cd /www/temp && tar -xzvf /www/temp/v0.2.19.tar.gz
 
 
-wget -O /www/temp "https://github.com/chaoslawful/lua-nginx-module/archive/v0.9.5.tar.gz"
-cd /www/temp/ && tar -xzvf v0.9.5.tar.gz
+wget -O /www/temp/v0.9.5.tar.gz "https://github.com/chaoslawful/lua-nginx-module/archive/v0.9.5.tar.gz"
+cd /www/temp && tar -xzvf v0.9.5.tar.gz
 
 
-wget -O /www/temp "http://nginx.org/download/nginx-1.4.6.tar.gz"
-cd /www/temp/ && tar -xzvf nginx-1.4.6.tar.gz
+wget -O /www/temp/nginx-1.4.6.tar.gz "http://nginx.org/download/nginx-1.4.6.tar.gz"
+cd /www/temp && tar -xzvf nginx-1.4.6.tar.gz
 
 
 
@@ -30,15 +31,16 @@ sudo apt-get install --reinstall libc-bin
 sudo apt-get install -f
 sudo cp /www/temp/unpackdir/sbin/ldconfig /root/
 
-//копируем ldconfig (и .real) в root. если ошибка в  след строке, То и в /www/temp
+#копируем ldconfig (и .real) в root. если ошибка в  след строке, То и в /www/temp
 sudo apt-get install libpcre3 libpcre3-dev
 sudo apt-get install libssl-dev
 export LUAJIT_LIB=/usr/local/lib
 export LUAJIT_INC=/usr/local/include/luajit-2.0
 
-cd /www/temp/nginx-1.4.6 && ./configure --prefix=/opt/nginx --add-module=/www/temp/ngx_devel_kit-0.2.19 --add-module=/www/temp/lua-nginx-module-0.9.5
-sudo make
-sudo make install
+cd /www/temp/nginx-1.4.6 && ./configure --prefix=/etc/nginx --add-module=/www/temp/ngx_devel_kit-0.2.19 --add-module=/www/temp/lua-nginx-module-0.9.5
+cd /www/temp/nginx-1.4.6 && sudo make
+cd /www/temp/nginx-1.4.6 && sudo make install
 
+cp /www/temp/nginx-1.4.6/objs/nginx /usr/sbin/
 sudo service nginx start
 
