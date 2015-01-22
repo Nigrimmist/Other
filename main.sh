937c7049e34014308be2236c365c1aad9b1af6b7
Nigrimmist <Nigrimmist@gmail.com>
2015-01-22 14:47:42 +0200

    - lsyncd removed

--- main.sh
+++ main.sh
@@ -21,22 +21,14 @@ sudo apt-get -y install htop
 
 
 #download scripts
-wget -O /www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
+#wget -O /www/scripts/lsyncd.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/lsyncd.sh"
 wget -O /www/scripts/nginx.sh "https://raw.githubusercontent.com/Nigrimmist/Other/master/Scripts/nginx.sh"
-chmod +x /www/scripts/lsyncd.sh
+#chmod +x /www/scripts/lsyncd.sh
 chmod +x /www/scripts/nginx.sh
 
-sudo /www/scripts/lsyncd.sh #sync
+#sudo /www/scripts/lsyncd.sh #sync
 sudo /www/scripts/nginx.sh #nginx
 
-#ftp
-#wget -O /www/configs/vsftpd.conf "https://raw.githubusercontent.com/Nigrimmist/Other/master/Configs/vsftpd.conf"
-#sudo apt-get install vsftpd 
-#sudo useradd -d /www/gifs -m ftpuser
-#echo "FTP PASSWORD : "
-#sudo passwd ftpuser
-#chown ftpuser:ftpuser /www/gifs
-#cp -f /www/configs/vsftpd.conf /etc/
 
 #setup ssh upload user
 sudo adduser --no-create-home $uploadFileUser
